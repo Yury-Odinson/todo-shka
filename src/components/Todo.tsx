020351eb6932b2728946a351387c51bab2f1d22e
Yury <yury.odinson@gmail.com>
2023-11-04 14:44:32 +0300

    feat: update token access

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,7 +1,12 @@
+import { checkToken, tokenRefresh } from "../tools/authorization"
+
 export const Todo = () => {
+
+
     return (
         <div className="todo-container">
             <h1>hello</h1>
+            <button onClick={() => checkToken(tokenRefresh)}>update</button>
         </div>
     )
 }
--- src/tools/authorization.ts
+++ src/tools/authorization.ts
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
 import { AuthUser, NewTokens } from "./types";
 
 const userAuthURL = "https://studapi.teachmeskills.by/auth/jwt/create/"
+const userRefreshToken = "https://studapi.teachmeskills.by//auth/jwt/refresh/"
 
-export let tokenRefresh = localStorage.getItem("tokenRefresh") || ""
+
 export let tokenAccess = localStorage.getItem("tokenAccess") || ""
+export let tokenRefresh = localStorage.getItem("tokenRefresh") || ""
 
 // get user tokens
 export const authUser = async (user: AuthUser) => {
@@ -19,6 +20,17 @@ export const authUser = async (user: AuthUser) => {
 
 // save tokens in the localStorage
 export const saveTokens = ({ access, refresh }: NewTokens) => {
-    localStorage.setItem("access", access)
-    localStorage.setItem("refresh", refresh)
+    localStorage.setItem("tokenAccess", access)
+    localStorage.setItem("tokenRefresh", refresh)
+}
+
+export const checkToken = async (refresh: string) => {
+    const refreshURL = new URL(userRefreshToken)
+    const response = await fetch(refreshURL, {
+        method: "POST",
+        body: JSON.stringify(new Object({ "refresh": refresh })),
+        headers: { "Content-Type": "application/json" }
+    })
+    const newToken = await response.json()
+    return localStorage.setItem("tokenAccess", newToken.access)
 }
