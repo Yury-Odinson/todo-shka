6f2a462eead4e0085039146e44ece84c31768ccd
Yury <yury.odinson@gmail.com>
2023-11-05 13:05:36 +0300

    feat: change task

--- src/components/TaskElement.tsx
+++ src/components/TaskElement.tsx
@@ -5,6 +5,12 @@ import { TaskContext, setLocalStorage } from "../tools/store";
 export const TaskElement = (taskProps: { task: Task }) => {
 
     const [completed, setCompleted] = useState<boolean>(taskProps.task.completed)
+    const [readOnly, setReadOnly] = useState<boolean>(true)
+
+    const [name, setName] = useState(taskProps.task.name)
+    const [description, setDescription] = useState(taskProps.task.description)
+    const [date, setDate] = useState(taskProps.task.date)
+    const [time, setTime] = useState(taskProps.task.time)
 
     const getTaskDate = () => {
         const day = new Date(Number(taskProps.task.id)).getDate()
@@ -13,10 +19,10 @@ export const TaskElement = (taskProps: { task: Task }) => {
         return `${day}.${month}.${year}`
     }
 
-    const { tasks } = useContext(TaskContext)
-    const { setTasks } = useContext(TaskContext)
+    const { tasks } = useContext(TaskContext)       // tasks array in the context
+    const { setTasks } = useContext(TaskContext)    // function of the changes context
 
-    const indexOf = tasks.indexOf(taskProps.task)
+    const indexOf = tasks.indexOf(taskProps.task)   // declarate index task on the array
 
     const removeTask = () => {
         tasks.splice(indexOf, 1)
@@ -30,17 +36,40 @@ export const TaskElement = (taskProps: { task: Task }) => {
         setLocalStorage(tasks)
     }
 
+    const editTask = () => {
+        setReadOnly(!readOnly)
+        const updated: any = {
+            name: name,
+            description: description,
+            date: date,
+            time: time
+        }
+        const task: any = tasks[indexOf]
+        // update cpecific object keys
+        for (const key in updated) {
+            if (updated.hasOwnProperty(key)) {
+                task[key] = updated[key]
+            }
+        }
+        // write changes in the context & update localStorage
+        setTasks(tasks)
+        setLocalStorage(tasks)
+    }
+
     return (
         <div className="todo-item">
-            <div className="table__isCompleted"><input type="checkbox" checked={completed} onChange={() => changeCompleted()} /></div>
-            <div className="table__name">{taskProps.task.name}</div>
-            <div className="table__description">{taskProps.task.description}</div>
-            <div className="table__deadline">
-                <input type="date" value={taskProps.task.date} readOnly />
-                <input type="time" value={taskProps.task.time} readOnly />
+            <div className="table__isCompleted">
+                <input type="checkbox" checked={completed} onChange={() => changeCompleted()} />
             </div>
-            <span className="todo-item__date">task create: {getTaskDate()}</span>
+            <input className="table__name" readOnly={readOnly} value={name} onChange={(e) => setName(e.target.value)} />
+            <input className="table__description" readOnly={readOnly} value={description} onChange={(e) => setDescription(e.target.value)} />
+            <div className="table__deadline">
+                <input type="date" value={date} readOnly={readOnly} onChange={(e) => setDate(e.target.value)} />
+                <input type="time" value={time} readOnly={readOnly} onChange={(e) => setTime(e.target.value)} />
+            </div>
             <button onClick={() => removeTask()}>X</button>
+            <button onClick={() => editTask()}>change</button>
+            <span className="todo-item__date">task create: {getTaskDate()}</span>
         </div>
     )
 }
