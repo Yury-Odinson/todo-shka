80f4a5fb5a32a705ad7a5a9269339e7b428be19c
Yury <yury.odinson@gmail.com>
2023-11-06 09:43:35 +0300

    feat: styles for new task window

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -25,19 +25,24 @@ export const NewTask = ({ isOpen }: NewTaskProps) => {
         <>
             <div className="new-task-background" onClick={() => isOpen()} />
             <div className="new-task">
-                <label>
+                <button className="new-task__close" onClick={() => isOpen()} />
+                <label className="new-task-label">
+                    <span className="todo-column__Clue">name</span>
                     <input type="text" placeholder="input name of the task" onChange={(e) => setInputName(e.target.value)} />
                 </label>
-                <label>
+                <label className="new-task-label">
+                    <span className="todo-column__Clue">description</span>
                     <input type="text" placeholder="input description of the task" onChange={(e) => setInputDescription(e.target.value)} />
                 </label>
-                <label>
+                <label className="new-task-label">
+                    <span className="todo-column__Clue">date</span>
                     <input type="date" onChange={(e) => setInputDate(e.target.value)} />
                 </label>
-                <label>
+                <label className="new-task-label">
+                    <span className="todo-column__Clue">time</span>
                     <input type="time" onChange={(e) => setInputTime(e.target.value)} />
                 </label>
-                <button onClick={() => {
+                <button className="new-task__button" onClick={() => {
                     addTask()
                 }}>add task</button>
             </div>
